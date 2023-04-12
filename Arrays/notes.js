b443f6a4ac8004c72217214d824318b8fe185409
Yeabsira Nigussie <nigusseyeabsira@gmail.com>
2023-04-12 12:49:23 +0300

    done with for loops in javascript

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -1,6 +1,33 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = ['Note 1', 'Note 2', 'Note 3', 'Note 4', 'Note 5']
 console.log(notes)
 
-console.log(notes.length)
+// console.log(notes.length)
 
-console.log(notes[1])
+// console.log(notes[1])
+
+// notes.push('attending meeting')
+
+// console.log('removed Note: '+ notes.shift())//pop() operator for front index
+// notes.unshift('it should be Note 1 here')// push() operator for front index
+
+//notes.splice(0, 1)// inclusive deletion from starting point to the end of an arrray
+//console.log(notes)
+//notes.splice(0,0, 'Note 1 is back')//so i can remove whate ever i want and enter whatevet i need
+
+console.log(notes)
+//notes.splice(0, 2, 'whare are you Note')// remove the first two and enter at first position
+console.log(notes)
+
+//remove note 3 and 4 and insert at note 1
+notes.splice(2, 2, 'who is removed')
+
+console.log(notes)
+
+//notes.splice() mainly remove item from specific index and upto specified index sequencially
+// can also insert value to the start of removed value
+
+
+notes.forEach(function (item, index){
+     console.log(index)
+     console.log(item)
+})
--- Arrays/todo.js
+++ Arrays/todo.js
@@ -8,4 +8,22 @@ const todo = [
     'go to small group program'
 ]
 console.log(`I have ${todo.length} things to do`)
-console.log('Todo list: '+ todo)
+//console.log('Todo list: '+ todo)
+
+// another challenge
+// Delete 3rd item from the list  
+// add item to the end of the list
+// remove the first element from the list 
+
+todo.splice(2, 1)
+todo.push('Attending Meeting')
+todo.shift()
+//console.log(todo)
+
+todo.forEach(function(item, index){
+    console.log(`${1 + index}, ${item}`)
+}) 
+// console.log()
+// for(let i =0; i<todo.length; i++){
+//     console.log(`${i+1}, ${todo[i]}`)
+// }
