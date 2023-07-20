3bd1fb59460d7aa4f18160907225bd83052756dc
YeabsiraNigusse <nigusseyeabsira@gmail.com>
2023-07-20 10:36:51 +0300

    working on section 7

--- todo-app/todo.js
+++ todo-app/todo.js
@@ -1,50 +1,17 @@
 //DOM - Document Objet Model
-let todos = []
-
-//getting notes from local storage if there is any stored
-
-const storedTodos = localStorage.getItem('todos')
-if (storedTodos !== null){
-    todos = JSON.parse(storedTodos)
-}
+const todos = getSavedTodos()
 
 const filters = {
     searchtodo: '', 
     hideCompleted: false
 }
 
-const renderTodo = function(todos, filters){
-    let filteredTodos = todos.filter(function (todo){
-        const searResult = todo.text.toLowerCase().includes(filters.searchtodo.toLowerCase())
-        const filteredResult = !filters.hideCompleted || !todo.completed
-        return searResult && filteredResult
-    })
-    //console.log(filteredTodos)
-    document.querySelector('#todo').innerHTML = ''
-
-    const getThingsTodo = function(todos){
-        return todos.filter(function (todo){
-             return !todo.completed
-         })
-     }
-     const p1 = document.createElement('h2');
-     p1.textContent = `You have ${getThingsTodo(todos).length} todos left`;
-     document.querySelector('#todo').appendChild(p1);
-
-    filteredTodos.forEach(function (todo){
-        let newElem = document.createElement('p')
-        newElem.textContent = todo.text
-        document.querySelector('#todo').appendChild(newElem)
-    })
-}
-
 renderTodo(todos, filters) 
-
 
 // handling user interaction
 document.querySelector('#add-form').addEventListener('submit', function (e){//accessing a user data from a form
     e.preventDefault()
-    //console.log(e.target.elements.newTodo.value)//helps to access specific finput of a form
+    
     if (e.target.elements.newTodo.value == ''){
         console.log('enter some todos')
     }else{
@@ -55,7 +22,8 @@ document.querySelector('#add-form').addEventListener('submit', function (e){//ac
     }
     
     console.log(todos) 
-    localStorage.setItem('todos', JSON.stringify(todos))
+    saveTodos(todos)
+
     renderTodo(todos, filters)
     e.target.elements.newTodo.value = ''
 })
--- todo-app/todos-function.js
+++ todo-app/todos-function.js
@@ -1,6 +1,61 @@
-const getSavedTodos = function(todos){
-    const localTodo = localStorage.getItem('todos')
-if (localTodo !==null){
-    todos = JSON.parse(localTodo)
+// getting todos from local storage
+
+const getSavedTodos = function (){
+    const todoJson = localStorage.getItem('todos')
+
+    if (todoJson !== null){
+        return JSON.parse(todoJson)
+    }else{
+        return []
+    }
 }
+
+// save todos
+
+const saveTodos = function(todos){
+    localStorage.setItem('todo', JSON.stringify(todos))
+}
+
+// getting remaining todos
+
+const getThingsTodo = function(todos){
+    return todos.filter(function (todo){
+         return !todo.completed
+     })
+ }
+
+ // show remaining todos in browser
+ const showReaminingTodos = function (todos){
+    const p1 = document.createElement('h2');
+     p1.textContent = `You have ${getThingsTodo(todos).length} todos left`;
+     document.querySelector('#todo').appendChild(p1);
+ }
+
+ // show filtered todos in browser
+
+ const getTodos = function (todo){
+        let newElem = document.createElement('p')
+        newElem.textContent = todo.text
+        document.querySelector('#todo').appendChild(newElem)
+ }
+ const getFilteredTodos = function (todos, filters){
+    return todos.filter(function (todo){
+        const searResult = todo.text.toLowerCase().includes(filters.searchtodo.toLowerCase())
+        const filteredResult = !filters.hideCompleted || !todo.completed
+        
+        return searResult && filteredResult
+    })
+ }
+
+
+ const renderTodo = function(todos, filters){
+    let filteredTodos = getFilteredTodos(todos, filters)
+    
+    document.querySelector('#todo').innerHTML = ''
+    
+    showReaminingTodos(todos)
+
+    filteredTodos.forEach(function (todo){
+        getTodos(todo)
+    })
 }
