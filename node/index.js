f1425579cd5b9b740e7fdf40e9dc08d0e1a1170a
YeabsiraNigusse <nigusseyeabsira@gmail.com>
2023-08-17 11:52:33 +0300

    learned about package mannage

---
+++ node/index.js
@@ -0,0 +1,11 @@
+
+
+var mysuper = require('superheroes')
+var supers = require('supervillains')
+
+var villains = supers.random()
+
+var heroName = mysuper.random()
+
+console.log(villains)
+//console.log(heroName)
