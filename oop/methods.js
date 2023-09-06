1c0208c7886c6d764f85f18f09cb5aab4119510c
YeabsiraNigusse <nigusseyeabsira@gmail.com>
2023-09-06 11:33:15 +0300

    learned how we make methods

---
+++ oop/methods.js
@@ -0,0 +1,29 @@
+
+
+const Person = function (firstName, lastName, age, likes = []){
+
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.likes = likes
+}
+
+Person.prototype.setName = function(fullName){
+    const Name = fullName.split(' ')
+    this.firstName = Name[0]
+    this.lastName = Name[1]
+}
+
+Person.prototype.getBio = function (){
+    let bio =  `Hi ${this.firstName} ${this.lastName}`
+
+    this.likes.forEach(function (like){
+        bio += ` you like ${like} `
+    })
+
+    return bio
+}
+
+const person1 = new Person('Yeab', 'lastName', 21, ['reading', 'codding'])
+person1.setName('Yeabsira Nigusse')
+console.log(person1.getBio())
