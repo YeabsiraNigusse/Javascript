6d1469ba3d8671f644c0e6b6172c554f4d5f8951
YeabsiraNigusse <nigusseyeabsira@gmail.com>
2023-09-06 12:01:10 +0300

    added  method to specific instance

--- oop/methods.js
+++ oop/methods.js
@@ -24,6 +24,16 @@ Person.prototype.getBio = function (){
     return bio
 }
 
+
+
 const person1 = new Person('Yeab', 'lastName', 21, ['reading', 'codding'])
 person1.setName('Yeabsira Nigusse')
 console.log(person1.getBio())
+
+const me = new Person('bereket', 'Nigusse', 18)
+
+me.getBio = function(){
+    return 'this is a  method specific to me'
+}
+
+console.log(me.getBio())
